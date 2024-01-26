afa6157884f210c01bd243c9c8e94739052bf805
Rahul <rjcode0@gmail.com>
2024-01-26 13:34:07 +0530

    new changes

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,26 +1,50 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name     = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-
-    class Meta:
-        ordering = ['-created_on']
+    STATUS_CHOICES = ((0, 'Draft'), (1, 'Published'),)
+    title       = models.CharField(max_length=200, unique=True)
+    slug        = models.SlugField(max_length=200, unique=True)
+    author      = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on  = models.DateTimeField(auto_now=True)
+    content     = models.TextField()
+    created_on  = models.DateTimeField(auto_now_add=True)
+    category    = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL, null=True, blank=True)
+    status      = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    likes       = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_likes')
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.post.title}"
+
+
+class Comment(models.Model):
+    post       = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    content    = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.post.title}"
+
 
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 from django.views import generic
@@ -7,6 +7,9 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+
+def index(request):
+    return render(request,'index.html')
 
 class PostDetail(generic.DetailView):
     model = Post
